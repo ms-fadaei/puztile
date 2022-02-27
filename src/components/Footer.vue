10b3c1d5f9d8c0a6eb701943e6db3947f8bd67bf
Mohammad Saleh Fadaei <54557683+ms-fadaei@users.noreply.github.com>
2022-02-27 12:25:28 +0330

    chore: update github link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,7 +13,7 @@ import { isDark, toggleDark } from '~/composables'
       class="icon-btn"
       i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/ms-fadaei/wordlemple"
+      href="https://github.com/ms-fadaei/puztile"
       target="_blank"
       title="GitHub"
     />
